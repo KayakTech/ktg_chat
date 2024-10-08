810e135e2d20b819ca2bce024a515582601a2653
Aggrey <39681748+Emmanuel-Aggrey@users.noreply.github.com>
2024-10-08 21:44:17 +0000

    first commit

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -2,7 +2,6 @@
 from sqlalchemy import Boolean, Column, String
 from sqlalchemy.orm import relationship
 from app.core.models import BaseModel
-# from app.chat.models import room_participant
 
 
 class User(BaseModel):
@@ -11,9 +10,4 @@ class User(BaseModel):
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
-    todos = relationship("Todo", back_populates="created_by")
-
     organisations = relationship("Organisation", back_populates='user')
-
-    # rooms = relationship("Room", secondary='room_participant',
-    #                      back_populates="participants")
